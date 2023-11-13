1db91381c3f497008f5ebe5236cb701172ff46ce
wuawuaque <52871757+wuawuaque@users.noreply.github.com>
2023-11-13 22:01:10 +0800

    tensorflow2x
    change-to-tensorflow2x

--- networks_pixelcnn.py
+++ networks_pixelcnn.py
@@ -1,125 +1,105 @@
 # Networks used in Pixel CNN
-
+# Networks used in Pixel CNN
 import tensorflow as tf
 import numpy as np
-
 def get_weights_pixelcnn(shape, name, horizontal=False, mask=None):
-	'''
-	shape: [filter_size, filter_size, img_channel, f_map] (filter_size should be odd)
-	horizontal: True for verticak stack; False for horizontal stack
-	mask: None for the filter with no mask; 
-	      'a' for masked filter in the first layer;
-	      'b' for masked filter in the second, third,... layers.
-	'''
-	weights_initializer = tf.contrib.layers.xavier_initializer()
-	W = tf.get_variable(name=name, shape=shape, dtype=tf.float32, initializer=weights_initializer)
-	if mask:
-		filter_mid_y = shape[0]//2
-		filter_mid_x = shape[1]//2 # [filter_mid_y, filter_mid_x] is the center of the filter
-		mask_filter = np.ones(shape=shape, dtype=np.float32)
-		if horizontal: 
-			mask_filter[filter_mid_y+1:, :, :, :] = 0.0
-			mask_filter[filter_mid_y, filter_mid_x+1:, :, :] = 0.0
-			if mask == 'a':
-				mask_filter[filter_mid_y, filter_mid_x, :, :] = 0.0
-		else: 
-			if mask == 'a':
-				mask_filter[filter_mid_y:, :, :, :] = 0.0
-			else:
-				mask_filter[filter_mid_y+1:, :, :, :] = 0.0
-		W *= mask_filter
-		# W = tf.multiply(W, mask_filter)
-	return W
-
+    '''
+    shape: [filter_size, filter_size, img_channel, f_map] (filter_size should be odd)
+    horizontal: True for verticak stack; False for horizontal stack
+    mask: None for the filter with no mask; 
+          'a' for masked filter in the first layer;
+          'b' for masked filter in the second, third,... layers.
+    '''
+    weights_initializer = tf.keras.initializers.GlorotNormal()
+    W = tf.Variable(initial_value=weights_initializer(shape=shape), name=name, dtype=tf.float32)
+    if mask:
+        filter_mid_y = shape[0]//2
+        filter_mid_x = shape[1]//2 # [filter_mid_y, filter_mid_x] is the center of the filter
+        mask_filter = np.ones(shape=shape, dtype=np.float32)
+        if horizontal: 
+            mask_filter[filter_mid_y+1:, :, :, :] = 0.0
+            mask_filter[filter_mid_y, filter_mid_x+1:, :, :] = 0.0
+            if mask == 'a':
+                mask_filter[filter_mid_y, filter_mid_x, :, :] = 0.0
+        else: 
+            if mask == 'a':
+                mask_filter[filter_mid_y:, :, :, :] = 0.0
+            else:
+                mask_filter[filter_mid_y+1:, :, :, :] = 0.0
+        W.assign(W * mask_filter)
+    return W
 def gated_conv_pixelcnn(W_shape_f, fan_in, horizontal, payload=None, mask=None):
-	'''
-	W_shape_f: [filter_size, filter_size, f_map] (filter_size should be odd)
-	fan_in: data to fit in
-	'''
-	# in_dim = tf.shape(fan_in)[-1]
-	in_dim = fan_in.get_shape()[-1]
-	W_shape = [W_shape_f[0], W_shape_f[1], in_dim, W_shape_f[2]]
-	b_shape = W_shape[2]
-
-	W_f = get_weights_pixelcnn(shape=W_shape, name="v_W", horizontal=horizontal, mask=mask)
-	W_g = get_weights_pixelcnn(shape=W_shape, name="h_W", horizontal=horizontal, mask=mask)
-	b_f_total = tf.get_variable(name="v_b", shape=b_shape, dtype=tf.float32, initializer=tf.zeros_initializer)
-	b_g_total = tf.get_variable(name="h_b", shape=b_shape, dtype=tf.float32, initializer=tf.zeros_initializer)
-
-	conv_f = tf.nn.conv2d(input=fan_in, filter=W_f, strides=[1,1,1,1], padding='SAME')
-	conv_g = tf.nn.conv2d(input=fan_in, filter=W_g, strides=[1,1,1,1], padding='SAME')
-
-	if payload is not None:
-		conv_f += payload
-		conv_g += payload
-
-	fan_out = tf.multiply(tf.tanh(conv_f + b_f_total), tf.sigmoid(conv_g + b_g_total))
-	return fan_out
+    '''
+    W_shape_f: [filter_size, filter_size, f_map] (filter_size should be odd)
+    fan_in: data to fit in
+    '''
+    in_dim = fan_in.shape[-1]
+    W_shape = [W_shape_f[0], W_shape_f[1], in_dim, W_shape_f[2]]
+    b_shape = W_shape[2]
+    W_f = get_weights_pixelcnn(shape=W_shape, name="v_W", horizontal=horizontal, mask=mask)
+    W_g = get_weights_pixelcnn(shape=W_shape, name="h_W", horizontal=horizontal, mask=mask)
+    b_f_total = tf.Variable(initial_value=tf.zeros(shape=b_shape), name="v_b", dtype=tf.float32)
+    b_g_total = tf.Variable(initial_value=tf.zeros(shape=b_shape), name="h_b", dtype=tf.float32)
+    conv_f = tf.nn.conv2d(input=fan_in, filters=W_f, strides=[1,1,1,1], padding='SAME')
 
 def simple_conv_pixelcnn(W_shape_f, fan_in, activation=True):
-	'''
-	W_shape_f: [filter_size, filter_size, f_map] (filter_size should be odd)
-	fan_in: data to fit in
-	'''
-	in_dim = fan_in.get_shape()[-1]
-	W_shape = [W_shape_f[0], W_shape_f[1], in_dim, W_shape_f[2]]
-	b_shape = W_shape_f[2]
+    '''
+    W_shape_f: [filter_size, filter_size, f_map] (filter_size should be odd)
+    fan_in: data to fit in
+    '''
+    in_dim = fan_in.shape[-1]
+    W_shape = [W_shape_f[0], W_shape_f[1], in_dim, W_shape_f[2]]
+    b_shape = W_shape_f[2]
+    W = get_weights_pixelcnn(shape=W_shape, name="W")
+    b = tf.Variable(initial_value=tf.zeros(shape=b_shape), name="b", dtype=tf.float32)
+    conv = tf.nn.conv2d(input=fan_in, filters=W, strides=[1,1,1,1], padding='SAME')
+    if activation:
+        fan_out = tf.nn.relu(tf.add(conv, b))
+    else:
+        fan_out = tf.add(conv, b)
+    return fan_out
 
-	W = get_weights_pixelcnn(shape=W_shape, name="W")
-	b = tf.get_variable(name="b", shape=b_shape, dtype=tf.float32, initializer=tf.zeros_initializer)
-
-	conv = tf.nn.conv2d(input=fan_in, filter=W, strides=[1,1,1,1], padding='SAME')
-	if activation:
-		fan_out = tf.nn.relu(tf.add(conv, b))
-	else:
-		fan_out = tf.add(conv, b)
-	return fan_out
+import tensorflow as tf
+from tensorflow.keras import layers
 
 def pixelcnn(inputs, num_layers_pixelcnn, fmaps_pixelcnn, num_embeddings, code_size):
-	inputs_shape = tf.shape(inputs)
-	inputs = tf.reshape(inputs, [inputs_shape[0], code_size, code_size, 1])
-	inputs = tf.cast(inputs, tf.float32)
+    inputs_shape = tf.shape(inputs)
+    inputs = tf.reshape(inputs, [inputs_shape[0], code_size, code_size, 1])
+    inputs = tf.cast(inputs, tf.float32)
 
-	v_stack_in = inputs
-	h_stack_in = inputs
+    v_stack_in = inputs
+    h_stack_in = inputs
 
-	for i in range(num_layers_pixelcnn):
-		filter_size = 3 if i > 0 else 7
-		mask = 'b' if i > 0 else 'a'
-		residual = True if i > 0 else False
-		i = str(i)
+    for i in range(num_layers_pixelcnn):
+        filter_size = 3 if i > 0 else 7
+        mask = 'b' if i > 0 else 'a'
+        residual = True if i > 0 else False
+        i = str(i)
 
-		with tf.variable_scope("v_stack_pixelcnn"+i):
-			v_stack = gated_conv_pixelcnn(W_shape_f=[filter_size, filter_size, fmaps_pixelcnn], fan_in=v_stack_in, horizontal=False, mask=mask)
-			v_stack_in = v_stack
+        v_stack = gated_conv_pixelcnn(W_shape_f=[filter_size, filter_size, fmaps_pixelcnn], fan_in=v_stack_in, horizontal=False, mask=mask)
+        v_stack_in = v_stack
 
-		with tf.variable_scope("v_stack_1_pixelcnn"+i):
-			v_stack_1 = simple_conv_pixelcnn(W_shape_f=[1, 1, fmaps_pixelcnn], fan_in=v_stack_in)
+        v_stack_1 = simple_conv_pixelcnn(W_shape_f=[1, 1, fmaps_pixelcnn], fan_in=v_stack_in)
 
-		with tf.variable_scope("h_stack_pixelcnn"+i):
-			h_stack = gated_conv_pixelcnn(W_shape_f=[filter_size, filter_size, fmaps_pixelcnn], fan_in=h_stack_in, horizontal=True, payload=v_stack_1, mask=mask)
+        h_stack = gated_conv_pixelcnn(W_shape_f=[filter_size, filter_size, fmaps_pixelcnn], fan_in=h_stack_in, horizontal=True, payload=v_stack_1, mask=mask)
 
-		with tf.variable_scope("h_stack_1_pixelcnn"+i):
-			h_stack_1 = simple_conv_pixelcnn(W_shape_f=[1, 1, fmaps_pixelcnn], fan_in=h_stack)
-			if residual:
-				h_stack_1 += h_stack_in
-			h_stack_in = h_stack_1
+        h_stack_1 = simple_conv_pixelcnn(W_shape_f=[1, 1, fmaps_pixelcnn], fan_in=h_stack)
+        if residual:
+            h_stack_1 += h_stack_in
+        h_stack_in = h_stack_1
 
-	with tf.variable_scope("fc_1_pixelcnn"):
-		fc1 = simple_conv_pixelcnn(W_shape_f=[1, 1, fmaps_pixelcnn], fan_in=h_stack_in)
-	with tf.variable_scope("fc_2_pixelcnn"):
-		fc2 = simple_conv_pixelcnn(W_shape_f=[1, 1, num_embeddings], fan_in=fc1, activation=False)
+    fc1 = simple_conv_pixelcnn(W_shape_f=[1, 1, fmaps_pixelcnn], fan_in=h_stack_in)
+    fc2 = simple_conv_pixelcnn(W_shape_f=[1, 1, num_embeddings], fan_in=fc1, activation=False)
 
-	dist = tf.distributions.Categorical(logits=fc2)
-	sampled_pixelcnn = dist.sample()
-	log_prob_pixelcnn = dist.log_prob(sampled_pixelcnn)
+    dist = tf.random.categorical(logits=fc2, num_samples=1)
+    sampled_pixelcnn = dist.sample()
+    log_prob_pixelcnn = dist.log_prob(sampled_pixelcnn)
 
-	inputs = tf.reshape(inputs, tf.shape(inputs)[:-1])
-	inputs = tf.cast(inputs, tf.int32)
-	loss_per_batch = tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=fc2, labels=inputs), axis=[1,2])
-	loss_pixelcnn = tf.reduce_mean(loss_per_batch)
+    inputs = tf.reshape(inputs, tf.shape(inputs)[:-1])
+    inputs = tf.cast(inputs, tf.int32)
+    loss_per_batch = tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=fc2, labels=inputs), axis=[1,2])
+    loss_pixelcnn = tf.reduce_mean(loss_per_batch)
 
-	return {'loss_pixelcnn': loss_pixelcnn,
-			'sampled_pixelcnn': sampled_pixelcnn,
-			'log_prob_pixelcnn': log_prob_pixelcnn}
-
+    return {'loss_pixelcnn': loss_pixelcnn,
+            'sampled_pixelcnn': sampled_pixelcnn,
+            'log_prob_pixelcnn': log_prob_pixelcnn}
--- vqvae1_withPixelCNNprior_mnist.py
+++ vqvae1_withPixelCNNprior_mnist.py
@@ -42,10 +42,9 @@ grad_clip_pixelcnn = 1.0
 
 #--------------------------------------------------------------------------
 # Placeholder
-x = tf.placeholder(tf.float32, shape=(None, image_size, image_size, num_channel))
-data_pixelcnn = tf.placeholder(tf.int32, shape=(None, 7, 7)) # Train
-sampled_code_pixelcnn = tf.placeholder(tf.int32, shape=(None, 7, 7)) # Plot
-
+x = tf.keras.Input(shape=(image_size, image_size, num_channel))
+data_pixelcnn = tf.keras.Input(shape=(None, 7, 7)) # Train
+sampled_code_pixelcnn = tf.keras.Input(shape=(None, 7, 7)) # Plot
 #--------------------------------------------------------------------------
 # Data
 
@@ -61,189 +60,133 @@ def convert_batch_to_image_grid(images, image_size=image_size, num_channel=num_c
                 .reshape(10 * image_size, 10 * image_size))
     return reshaped + 0.5
 
+from tensorflow.keras.datasets import mnist
+(mnist_train_images, _), (mnist_test_images, _) = mnist.load_data()
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data')
-
-mnist_train_images = mnist.train.images.reshape([-1, 28, 28, 1]) # shape=[55000, 28, 28, 1]
-mnist_test_images = mnist.test.images.reshape([-1, 28, 28, 1])   # shape=[10000, 28, 28, 1]
+mnist_train_images = mnist_train_images.reshape([-1, 28, 28, 1]) # shape=[55000, 28, 28, 1]
+mnist_test_images = mnist_test_images.reshape([-1, 28, 28, 1])   # shape=[10000, 28, 28, 1]
 
 data_variance = np.var(mnist_train_images)
 
 train_dataset = (tf.data.Dataset.from_tensor_slices(mnist_train_images)
-					.map(cast_and_normalise_images)
-        			.shuffle(10000)
-        			.repeat()
-        			.batch(batch_size))
+                    .map(cast_and_normalise_images)
+                    .shuffle(10000)
+                    .repeat()
+                    .batch(batch_size))
 test_dataset = (tf.data.Dataset.from_tensor_slices(mnist_test_images)
-        		.map(cast_and_normalise_images)
-        		.repeat()
-        		.batch(batch_size))
-train_iterator = train_dataset.make_one_shot_iterator()
-test_iterator  = test_dataset.make_one_shot_iterator()
-train_dataset_batch_tf = train_iterator.get_next()
-test_dataset_batch_tf  = test_iterator.get_next()
+                .map(cast_and_normalise_images)
+                .repeat()
+                .batch(batch_size))
+train_dataset_batch_tf = next(iter(train_dataset))
+test_dataset_batch_tf  = next(iter(test_dataset))
 print("Data loading is finished...")
+
+
+
 
 
 #--------------------------------------------------------------------------
 # Training process
 
 #--- Train process - vqvae1
+#--- Train process - vqvae1
+# Training process
+#--- Train process - vqvae1
 z = vqvae_nets.encoder(x, num_hiddens, num_residual_layers, num_residual_hiddens)
 with tf.variable_scope('to_vq'):
   z = vqvae_nets.conv2d(z, fmaps=embedding_dim, kernel=1, strides=1)
 vq_output = vq.vector_quantizer(z, embedding_dim, num_embeddings, commitment_cost)
 x_recon = vqvae_nets.decoder(vq_output["quantized"], num_hiddens, num_residual_layers, num_residual_hiddens, image_size, num_channel)
-
 recon_error = tf.reduce_mean((x_recon - x)**2) / data_variance  # Normalized MSE
 loss = recon_error + vq_output["loss"]
 perplexity = vq_output["perplexity"] 
-
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss)
 
 #--- Train process - PixelCNN for priors
 train_databatch_pixelcnn = vq_output["encoding_indices"]
-
 pixelcnn_output = pixelcnn_nets.pixelcnn(data_pixelcnn, num_layers_pixelcnn, fmaps_pixelcnn, num_embeddings, code_size)
 loss_pixelcnn = pixelcnn_output["loss_pixelcnn"]
 sampled_pixelcnn_train = pixelcnn_output["sampled_pixelcnn"]
-
-# optimizer_pixelcnn = tf.train.RMSPropOptimizer(learning_rate_pixelcnn).minimize(loss_pixelcnn)
-trainer_pixelcnn = tf.train.RMSPropOptimizer(learning_rate=learning_rate_pixelcnn)
-gradients_pixelcnn = trainer_pixelcnn.compute_gradients(loss_pixelcnn)
-clipped_gradients_pixelcnn = map(lambda gv: gv if gv[0] is None else [tf.clip_by_value(gv[0], -grad_clip_pixelcnn, grad_clip_pixelcnn), gv[1]], gradients_pixelcnn)
-# clipped_gradients_pixelcnn = [(tf.clip_by_value(_[0], -grad_clip_pixelcnn, grad_clip_pixelcnn), _[1]) for _ in gradients_pixelcnn]
+trainer_pixelcnn = tf.optimizers.RMSprop(learning_rate=learning_rate_pixelcnn)
+gradients_pixelcnn = trainer_pixelcnn.get_gradients(loss_pixelcnn, tf.trainable_variables())
+clipped_gradients_pixelcnn = [(tf.clip_by_value(grad, -grad_clip_pixelcnn, grad_clip_pixelcnn), var) for grad, var in gradients_pixelcnn]
 optimizer_pixelcnn = trainer_pixelcnn.apply_gradients(clipped_gradients_pixelcnn)
 
 #--- For plots
 vq_output_pixelcnn = vq.vector_quantizer(z, embedding_dim, num_embeddings, commitment_cost, only_lookup=True, inputs_indices=sampled_code_pixelcnn)
 x_recon_pixelcnn = vqvae_nets.decoder(vq_output_pixelcnn["quantized"], num_hiddens, num_residual_layers, num_residual_hiddens, image_size, num_channel)
-
 test_vq_output = vq.vector_quantizer(z, embedding_dim, num_embeddings, commitment_cost, random_gen=True)
 test_recon = vqvae_nets.decoder(test_vq_output["quantized"], num_hiddens, num_residual_layers, num_residual_hiddens, image_size, num_channel)
 test_recon_rand = vqvae_nets.decoder(test_vq_output["rand_quantized"], num_hiddens, num_residual_layers, num_residual_hiddens, image_size, num_channel)
 test_recon_near = vqvae_nets.decoder(test_vq_output["near_quantized"], num_hiddens, num_residual_layers, num_residual_hiddens, image_size, num_channel)
 
-
-#--------------------------------------------------------------------------
-# Train
-
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
-
 #--- Train vqvae1
 train_res_recon_error = []
 train_res_perplexity = []
 train_res_loss = []
-for i in range(num_training_updates):
-  train_data_batch = sess.run(train_dataset_batch_tf)
-  train_feed_dict = {x: train_data_batch}
-  train_res = sess.run([train_op, x_recon, recon_error, loss, perplexity], feed_dict=train_feed_dict)
-  
-  train_res_recon_error.append(train_res[2])
-  train_res_loss.append(train_res[3])
-  train_res_perplexity.append(train_res[4])
 
+for i in range(num_training_updates):
+  train_data_batch = next(iter(train_dataset))
+  with tf.GradientTape() as tape:
+    z = vqvae_nets.encoder(x, num_hiddens, num_residual_layers, num_residual_hiddens)
+    z = vqvae_nets.conv2d(z, fmaps=embedding_dim, kernel=1, strides=1)
+    vq_output = vq.vector_quantizer(z, embedding_dim, num_embeddings, commitment_cost)
+    x_recon = vqvae_nets.decoder(vq_output["quantized"], num_hiddens, num_residual_layers, num_residual_hiddens, image_size, num_channel)
+    recon_error = tf.reduce_mean((x_recon - x)**2) / data_variance  # Normalized MSE
+    loss = recon_error + vq_output["loss"]
+    perplexity = vq_output["perplexity"] 
+  gradients = tape.gradient(loss, tf.trainable_variables())
+  optimizer.apply_gradients(zip(gradients, tf.trainable_variables()))
+  
+  train_res_recon_error.append(recon_error)
+  train_res_loss.append(loss)
+  train_res_perplexity.append(perplexity)
   if i == 0 or (i+1) % 100 == 0:
     print('%d iterations | loss:  %.3f | recon_error: %.3f | perplexity: %.3f ' % (i+1, np.mean(train_res_loss[-100:]), np.mean(train_res_recon_error[-100:]), np.mean(train_res_perplexity[-100:])))
-    x_recon_img = train_res[1]
-
-    test_data_batch = sess.run(test_dataset_batch_tf)
-    test_feed_dict = {x: test_data_batch}
-    [test_recon_img, test_recon_img_rand, test_recon_img_near] = sess.run([test_recon, test_recon_rand, test_recon_near], feed_dict=test_feed_dict)
-
-    f = plt.figure(figsize=(16,24))
-    ax = f.add_subplot(3,2,1)
-    ax.imshow(convert_batch_to_image_grid(train_data_batch), interpolation='nearest', cmap ='gray')
-    ax.set_title('training data originals')
-    plt.axis('off')
-
-    ax = f.add_subplot(3,2,2)
-    ax.imshow(convert_batch_to_image_grid(x_recon_img), interpolation='nearest', cmap ='gray')
-    ax.set_title('training data reconstructions')
-    plt.axis('off')
-
-    ax = f.add_subplot(3,2,3)
-    ax.imshow(convert_batch_to_image_grid(test_data_batch), interpolation='nearest', cmap ='gray')
-    ax.set_title('test data originals')
-    plt.axis('off')
-
-    ax = f.add_subplot(3,2,4)
-    ax.imshow(convert_batch_to_image_grid(test_recon_img), interpolation='nearest', cmap ='gray')
-    ax.set_title('test data reconstructions')
-    plt.axis('off')
-
-    ax = f.add_subplot(3,2,5)
-    ax.imshow(convert_batch_to_image_grid(test_recon_img_near), interpolation='nearest', cmap ='gray')
-    ax.set_title('test data recon by near encoder')
-    plt.axis('off')
-
-    ax = f.add_subplot(3,2,6)
-    ax.imshow(convert_batch_to_image_grid(test_recon_img_rand), interpolation='nearest', cmap ='gray')
-    ax.set_title('data recon by random encoder')
-    plt.axis('off')
-
-    plt.savefig(save_res_dir+'/reconstructions_iter_'+str(i+1)+'.png')
-    plt.close()
-
-
-f = plt.figure(figsize=(24,8))
-ax = f.add_subplot(1,3,1)
-ax.plot(train_res_recon_error)
-ax.set_yscale('log')
-ax.set_title('train recon_error.')
-
-ax = f.add_subplot(1,3,2)
-ax.plot(train_res_loss)
-ax.set_yscale('log')
-ax.set_title('loss.')
-
-ax = f.add_subplot(1,3,3)
-ax.plot(train_res_perplexity)
-ax.set_title('Average codebook usage (perplexity).')
-
-plt.savefig(save_res_dir+"/loss.png")
-plt.close()
+    x_recon_img = x_recon
+    test_data_batch = next(iter(test_dataset))
+    z = vqvae_nets.encoder(x, num_hiddens, num_residual_layers, num_residual_hiddens)
+    z = vqvae_nets.conv2d(z, fmaps=embedding_dim, kernel=1, strides=1)
+    test_vq_output = vq.vector_quantizer(z, embedding_dim, num_embeddings, commitment_cost)
+    test_recon = vqvae_nets.decoder(test_vq_output["quantized"], num_hiddens, num_residual_layers, num_residual_hiddens, image_size, num_channel)
+    test_recon_img = test_recon
 
 #--- Train pixelcnn
 train_loss_pixelcnn = []
 for i in range(num_training_updates_pixelcnn):
-    train_data_batch = sess.run(train_dataset_batch_tf)
-    train_databatch_pixelcnn_np = sess.run(train_databatch_pixelcnn, feed_dict={x: train_data_batch})
-    train_feed_dict = {x: train_data_batch, data_pixelcnn: train_databatch_pixelcnn_np}
-    train_res_pixelcnn = sess.run([loss_pixelcnn, optimizer_pixelcnn], feed_dict=train_feed_dict)
-
-    train_loss_pixelcnn.append(train_res_pixelcnn[0])
-
+    train_data_batch = next(iter(train_dataset))
+    train_databatch_pixelcnn_np = vq_output["encoding_indices"]
+    with tf.GradientTape() as tape:
+        pixelcnn_output = pixelcnn_nets.pixelcnn(data_pixelcnn, num_layers_pixelcnn, fmaps_pixelcnn, num_embeddings, code_size)
+        loss_pixelcnn = pixelcnn_output["loss_pixelcnn"]
+        sampled_pixelcnn_train = pixelcnn_output["sampled_pixelcnn"]
+    gradients_pixelcnn = tape.gradient(loss_pixelcnn, tf.trainable_variables())
+    clipped_gradients_pixelcnn = [(tf.clip_by_value(grad, -grad_clip_pixelcnn, grad_clip_pixelcnn), var) for grad, var in gradients_pixelcnn]
+    optimizer_pixelcnn.apply_gradients(clipped_gradients_pixelcnn)
+    
+    train_loss_pixelcnn.append(loss_pixelcnn)
     if i == 0 or (i+1) % 100 == 0:
-        print('%d iter_pixelcnn | loss:  %.3f ' % (i+1, train_res_pixelcnn[0]))
+        print('%d iter_pixelcnn | loss:  %.3f ' % (i+1, loss_pixelcnn))
         n_row = 10
         n_col = 10
         samples = np.zeros(shape=(n_row*n_col, code_size, code_size), dtype=np.int32)
         for j in range(code_size):
             for k in range(code_size):
-                data_dict = {data_pixelcnn: samples}
-                next_sample = sess.run(sampled_pixelcnn_train, feed_dict=data_dict)
+                next_sample = pixelcnn_output["sampled_pixelcnn"]
                 samples[:, j, k] = next_sample[:, j, k]
         samples.astype(np.int32)
         
-        feed_dict = {x: train_data_batch, sampled_code_pixelcnn: samples}
-        x_recon_pixelcnn_res = sess.run(x_recon_pixelcnn, feed_dict=feed_dict)
-
+        vq_output_pixelcnn = vq.vector_quantizer(z, embedding_dim, num_embeddings, commitment_cost, only_lookup=True, inputs_indices=sampled_code_pixelcnn)
+        x_recon_pixelcnn = vqvae_nets.decoder(vq_output_pixelcnn["quantized"], num_hiddens, num_residual_layers, num_residual_hiddens, image_size, num_channel)
         f = plt.figure(figsize=(6,16))
         ax = f.add_subplot(2,1,1)
         ax.imshow(convert_batch_to_image_grid(train_data_batch), interpolation='nearest', cmap ='gray')
         ax.set_title('training data originals')
         plt.axis('off')
-
         ax = f.add_subplot(2,1,2)
-        ax.imshow(convert_batch_to_image_grid(x_recon_pixelcnn_res), interpolation='nearest', cmap ='gray')
+        ax.imshow(convert_batch_to_image_grid(x_recon_pixelcnn), interpolation='nearest', cmap ='gray')
         ax.set_title('samples')
         plt.axis('off')
-
         plt.savefig(save_res_dir+'/sampled_iter_'+str(i+1)+'.png')
         plt.close()
-
